f976ef6884bc80a066e0dfee493ddd0dbdc1df32
Rohit Verma <rohitvermamrt17@gmail.com>
2026-08-17 01:45:03 +0530

    Remove help section from ai_financial_coach.py
    Removed help section with support links from documentation.

--- ai_financial_coach.py
+++ ai_financial_coach.py
@@ -956,13 +956,6 @@ def main():
         - All data is processed locally
         - No financial information is stored or transmitted
         - Secure API communication with Google's services
-        
-        ### Need Help?
-        
-        For support or questions:
-        - Check the [documentation](https://github.com/Shubhamsaboo/awesome-llm-apps)
-        - Report issues on [GitHub](https://github.com/Shubhamsaboo/awesome-llm-apps/issues)
-        """)
 
 if __name__ == "__main__":
     main()
